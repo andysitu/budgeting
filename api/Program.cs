45e6af07c98d5db2ea193cbd6a2f2bc6d448ea9d
Andy Situ <and.situ@gmail.com>
2024-05-05 23:55:03 -0700

    Revert "Change to Vite JS"
    This reverts commit 287f8bbe3589f1b3bc552eacd89acb24eb53dab8.

--- api/Program.cs
+++ api/Program.cs
@@ -47,7 +47,7 @@ app.MapIdentityApi<IdentityUser>();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/pizzas", async (ApplicationDbContext db) => await db.Pizzas.ToListAsync()).RequireAuthorization();
+app.MapGet("/pizzas", async (ApplicationDbContext db) => await db.Pizzas.ToListAsync());
 
 app.MapPost("/pizza", async (ApplicationDbContext db, Pizza pizza) => {
     await db.Pizzas.AddAsync(pizza);
