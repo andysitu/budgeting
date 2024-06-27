5dc5b4f18a2ba33af0631e3193ad646ad0e02953
Andy Situ <and.situ@gmail.com>
2024-06-27 00:15:08 -0700

    Create PurchasesController.cs

---
+++ api/controllers/PurchasesController.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Budgeting.Data;
+using Budgeting.Models;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("purchase")]
+[ApiController]
+public class PurchasesController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public PurchasesController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("purchases")]
+    public Task<List<Purchase>> ListPurchases()
+    {
+        Task<List<Purchase>> purchases = _context.Purchases.ToListAsync();
+
+        return purchases;
+    }
+}
