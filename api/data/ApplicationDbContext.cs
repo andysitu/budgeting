3ef5eb68a8c9f2be03d933ddf3d0662f5fadbef8
Andy Situ <and.situ@gmail.com>
2024-06-27 00:14:52 -0700

    Add Purchases to DBContext

--- api/data/ApplicationDbContext.cs
+++ api/data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Budgeting.Data
 {
-    public class AppUser : IdentityUser {}
+    public class AppUser : IdentityUser { }
 
 
     public class ApplicationDbContext : IdentityDbContext<AppUser>
@@ -14,6 +14,9 @@ namespace Budgeting.Data
         { }
 
         public DbSet<Budgeting.Models.Pizza> Pizzas { get; set; }
+        public DbSet<Budgeting.Models.Vendor> Vendors { get; set; }
+        public DbSet<Budgeting.Models.Purchase> Purchases { get; set; }
+        public DbSet<Budgeting.Models.PurchaseItem> PurchaseItems { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             => optionsBuilder.UseNpgsql("Host=localhost:5432;Database=budgeting;Username=budgetuser;Password=123abc");
