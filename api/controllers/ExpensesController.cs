3579a8a6ca56b88cdcb892afb989f0c6209da023
Andy Situ <and.situ@gmail.com>
2024-06-29 14:29:07 -0700

    Create ExpensesController.cs

---
+++ api/controllers/ExpensesController.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Budgeting.Data;
+using Budgeting.Models;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("expenses")]
+[ApiController]
+public class ExpensesController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public ExpensesController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("expenses")]
+    public Task<List<Purchase>> ListPurchases()
+    {
+        Task<List<Purchase>> purchases = _context.Purchases.ToListAsync();
+
+        return purchases;
+    }
+}
