de7939d889bb24b6e931637175d2b6891222871f
Andy Situ <and.situ@gmail.com>
2024-06-29 14:35:12 -0700

    Add description to Vendor

--- api/models/Vendor.cs
+++ api/models/Vendor.cs
@@ -7,6 +7,7 @@ namespace Budgeting.Models
     {
         public long Id { get; set; }
         public required string Name { get; set; }
+        public string Description { get; set; } = "";
         public required ICollection<Purchase> Purchases { get; set; } = new List<Purchase>();
         public required ICollection<Expense> Expenses { get; set; } = new List<Expense>();
     }
