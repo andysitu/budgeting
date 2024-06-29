38a8146314dcc5aaa7dff171dc6112f03c172d39
Andy Situ <and.situ@gmail.com>
2024-06-29 14:59:55 -0700

    Make Date nullable for Money

---
+++ api/models/Income.cs
@@ -0,0 +1,9 @@
+using Budgeting.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Budgeting.Models
+{
+    public class Income : Money
+    {
+    }
+}
--- api/models/Money.cs
+++ api/models/Money.cs
@@ -8,6 +8,6 @@ namespace Budgeting.Models
         public long Id { get; set; }
         public string? Description { get; set; }
         public decimal Amount { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime? Date { get; set; }
     }
 }
