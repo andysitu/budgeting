01d204fedad236741c24f8c6593d6f43df2c9235
Andy Situ <and.situ@gmail.com>
2024-06-30 23:37:47 -0700

    Add redux StoreProvider

--- ui/app/layout.tsx
+++ ui/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Wrapper from "./components/Wrapper";
+import Navbar from "./components/navigation/navbar";
+import StoreProvider from "./StoreProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Wrapper>{children}</Wrapper>
+        <StoreProvider>
+          <Navbar />
+          <Wrapper>{children}</Wrapper>
+        </StoreProvider>
       </body>
     </html>
   );
