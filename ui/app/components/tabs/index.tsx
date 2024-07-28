74ea9a4ef179ac1e572522b548fef52f0b1584fe
Andy Situ <and.situ@gmail.com>
2024-07-27 23:54:32 -0700

    Dummy Tabs element

---
+++ ui/app/components/tabs/index.tsx
@@ -0,0 +1,29 @@
+"usee client";
+
+import { ReactElement } from "react";
+import styles from "./style.module.css";
+
+type TabsProps = {
+  elements: Record<string, ReactElement>;
+};
+
+function Tabs({ elements }: TabsProps) {
+  if (Object.keys(elements).length === 0) {
+    return null;
+  }
+
+  const inputs = [],
+    displayElements = [];
+
+  for (const [name, element] of Object.entries(elements)) {
+    inputs.push(
+      <div>
+        <button>{name}</button>
+      </div>
+    );
+  }
+
+  return inputs;
+}
+
+export default Tabs;
--- ui/app/page.tsx
+++ ui/app/page.tsx
@@ -1,10 +1,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Tabs from "./components/tabs";
 
 export default function Home() {
   return (
     <main>
-      <div>Test</div>
+      <div>
+        <Tabs
+          elements={{
+            test: <div></div>,
+          }}
+        />
+      </div>
     </main>
   );
 }
