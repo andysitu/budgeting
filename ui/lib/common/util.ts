556cdc4d36cf9de64541ebf5b85164d7e4d1bcbf
Andy Situ <and.situ@gmail.com>
2024-08-09 00:08:57 -0700

    Add useMount to util

--- ui/lib/common/util.ts
+++ ui/lib/common/util.ts
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
+
 function isEmptyObject(o: any | Record<string, any>) {
   if (!o) return true;
 
   return Object.keys(o).length === 0;
 }
 
-export { isEmptyObject };
+const useMount = (fn: () => void) => {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(fn, []);
+};
+
+export { isEmptyObject, useMount };
