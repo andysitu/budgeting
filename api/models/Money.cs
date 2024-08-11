a05b7da0aeb3ecd98ca5de28cd36b7febaca497c
Andy Situ <and.situ@gmail.com>
2024-08-10 17:21:56 -0700

    Add name to Money schema

--- api/models/Money.cs
+++ api/models/Money.cs
@@ -6,6 +6,7 @@ namespace Budgeting.Models
     public abstract class Money
     {
         public long Id { get; set; }
+        public string Name { get; set; } = "";
         public string? Description { get; set; }
         public decimal Amount { get; set; }
         public DateTime? Date { get; set; }
