47db68ffccab44219fe3811e86ffedaa0d35a4c1
Andy Situ <and.situ@gmail.com>
2024-08-14 23:17:02 -0700

    Fix Vendor to be optional

--- api/models/Expense.cs
+++ api/models/Expense.cs
@@ -5,7 +5,7 @@ namespace Budgeting.Models
 {
     public class Expense : Money
     {
-        public long VendorId { get; set; }
+        public long? VendorId { get; set; }
         public Vendor? Vendor { get; set; }
         public bool Settled { get; set; } = false;
     }
