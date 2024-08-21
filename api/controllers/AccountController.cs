28d6a83fdf361ec66e0bbf68bad227280ea998c0
Andy Situ <and.situ@gmail.com>
2024-08-21 00:25:49 -0700

    Clean up file

--- api/controllers/AccountController.cs
+++ api/controllers/AccountController.cs
@@ -1,27 +1,24 @@
 using Budgeting.Data;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using System.Net;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 [Authorize]
 [ApiController]
 [Route("account")]
 public class AccountController : Controller
 {
-    private readonly UserManager<AppUser> userManager;
+    private readonly UserManager<AppUser> _userManager;
 
     public AccountController(UserManager<AppUser> userManager)
     {
-        this.userManager = userManager;
+        this._userManager = userManager;
     }
 
     private async Task<AppUser?> GetCurrentUserAsync()
     {
-        AppUser? user = await userManager.GetUserAsync(HttpContext.User);
+        AppUser? user = await _userManager.GetUserAsync(HttpContext.User);
 
         return user;
     }
