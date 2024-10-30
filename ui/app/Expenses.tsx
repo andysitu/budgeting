76d4eccf133358a2e4245c996e545e540029a67d
Andy Situ <and.situ@gmail.com>
2024-10-30 00:25:48 -0700

    Remove "use client" from Expenses

--- ui/app/Expenses.tsx
+++ ui/app/Expenses.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
