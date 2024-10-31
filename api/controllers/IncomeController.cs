c8de78b38fe8e7060c6482f2acaf37dc36ea51ae
Andy Situ <and.situ@gmail.com>
2024-10-31 01:20:28 -0700

    API: delete income

--- api/controllers/IncomeController.cs
+++ api/controllers/IncomeController.cs
@@ -25,4 +25,27 @@ public class IncomeController : ControllerBase
 
         return incomes;
     }
+
+    [Authorize]
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteIncome(long? id)
+    {
+
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        string? userId = Utilites.getCurrentUserId(HttpContext);
+
+        Income? income = await _context.Income.Where(e => e.Id == id && e.AppUserId == userId).FirstAsync();
+
+        if (income != null)
+        {
+            _context.Income.Remove(income);
+        }
+
+        await _context.SaveChangesAsync();
+        return Ok(income?.Id);
+    }
 }
