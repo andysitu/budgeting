202241ceb66deade3b4796ebd9ef69694461dc74
Andy Situ <and.situ@gmail.com>
2024-10-31 23:49:13 -0700

    API: post & get by id income

--- api/controllers/IncomeController.cs
+++ api/controllers/IncomeController.cs
@@ -26,6 +26,45 @@ public class IncomeController : ControllerBase
         return incomes;
     }
 
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Income>> GetIncomeById(long id)
+    {
+        var userId = Utilites.getCurrentUserId(HttpContext);
+
+        Income? income = await _context.Income.Where(e => e.Id == id && e.AppUserId == userId).FirstAsync();
+
+        if (income == null)
+        {
+            return NotFound();
+        }
+
+        string incomeData = JsonSerializer.Serialize(income);
+
+        return Ok(incomeData);
+    }
+
+    [Authorize]
+    [HttpPost("")]
+    public async Task<ActionResult<Income>> CreateIncome(Income income)
+    {
+        string? userId = Utilites.getCurrentUserId(HttpContext);
+
+        if (string.IsNullOrEmpty(userId))
+        {
+            return Unauthorized();
+        }
+
+        income.AppUserId = userId;
+
+        _context.Income.Add(income);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetIncomeById), new
+        {
+            id = income.Id
+        }, income);
+    }
+
     [Authorize]
     [HttpDelete("{id}")]
     public async Task<ActionResult> DeleteIncome(long? id)
