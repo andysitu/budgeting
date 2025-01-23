d09a7cf4b3b3b8e0f6d3221dbc202477a847f84f
Andy Situ <and.situ@gmail.com>
2025-01-22 23:56:56 -0800

    Api: forgot to include ExpenseType enum to Expense

--- api/models/Expense.cs
+++ api/models/Expense.cs
@@ -1,10 +1,17 @@
 namespace Budgeting.Models
 {
+    public enum ExpenseType
+    {
+        Purchase
+    }
+
     public class Expense : Money
     {
         public long? VendorId { get; set; }
         public Vendor? Vendor { get; set; }
         public bool Settled { get; set; } = false;
+
+        public ExpenseType? ExpenseType { get; set; } = null;
 
         public virtual ICollection<ExpenseItem> ExpenseItems { get; set; } = [];
     }
