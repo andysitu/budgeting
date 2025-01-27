2189f101af45e7698d112a6cade5b945dd3a5ca7
Andy Situ <and.situ@gmail.com>
2025-01-26 23:43:03 -0800

    DB: add IncomeType

--- api/models/Income.cs
+++ api/models/Income.cs
@@ -1,8 +1,13 @@
-﻿namespace Budgeting.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Budgeting.Models
 {
     public class Income : Money
     {
         public long? VendorId { get; set; }
         public Vendor? Vendor { get; set; }
+        public long IncomeTypeId { get; set; }
+        [ForeignKey(nameof(IncomeTypeId))]
+        public virtual IncomeType IncomeType { get; set; } = null!;
     }
 }
