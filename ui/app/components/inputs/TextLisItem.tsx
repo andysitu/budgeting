c2b078f78730b7a62085e3801756bd216b8e8d08
Andy Situ <and.situ@gmail.com>
2025-11-16 00:39:02 -0800

    UI: add ref to TextListItem

--- ui/app/components/inputs/TextLisItem.tsx
+++ ui/app/components/inputs/TextLisItem.tsx
@@ -1,42 +1,47 @@
 import { isEmptyObject } from "@/lib/common/util";
 import styles from "./textlistitem.module.css";
 import ListItem from "./ListItem";
+import { forwardRef } from "react";
 
-interface TextListItemProps {
+interface TextListItemProps
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "onChange"> {
   label: string;
   value: string;
   onChange: (value: string) => void;
   type?: string;
   containerStyle?: Record<string, any>;
+  // For rest of props going to input
+  [key: string]: any;
 }
 
-function TextListItem({
-  label,
-  value,
-  onChange,
-  type = "text",
-  containerStyle,
-}: TextListItemProps) {
-  const divStyle = {
-    display: "grid",
-    gridTemplateColumns: "1fr 1fr",
-  };
+const TextListItem = forwardRef<HTMLInputElement, TextListItemProps>(
+  function TextListItemWithRef(
+    { label, value, onChange, type = "text", containerStyle, ...rest },
+    ref
+  ) {
+    const divStyle = {
+      display: "grid",
+      gridTemplateColumns: "1fr 1fr",
+    };
 
-  if (!isEmptyObject(containerStyle)) {
-    Object.assign(divStyle, containerStyle);
+    if (!isEmptyObject(containerStyle)) {
+      Object.assign(divStyle, containerStyle);
+    }
+
+    return (
+      <ListItem label={label} containerStyle={divStyle}>
+        <input
+          value={value}
+          onChange={(e) => {
+            onChange(e.target.value);
+          }}
+          type={type}
+          ref={ref}
+          {...rest}
+        />
+      </ListItem>
+    );
   }
-
-  return (
-    <ListItem label={label} containerStyle={divStyle}>
-      <input
-        value={value}
-        onChange={(e) => {
-          onChange(e.target.value);
-        }}
-        type={type}
-      />
-    </ListItem>
-  );
-}
+);
 
 export default TextListItem;
