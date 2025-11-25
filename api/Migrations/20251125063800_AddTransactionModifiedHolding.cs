63cdcd19a777a14aec816e7c915b2d331f1651d9
Andy Situ <and.situ@gmail.com>
2025-11-24 22:38:39 -0800

    DB: add Transaction ModifiedHolding

---
+++ api/Migrations/20251125063800_AddTransactionModifiedHolding.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTransactionModifiedHolding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ModifiedHolding",
+                table: "Transactions",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false,
+                comment: "If the transaction affected the holding price/shares.");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ModifiedHolding",
+                table: "Transactions");
+        }
+    }
+}
--- api/models/accounts/Transaction.cs
+++ api/models/accounts/Transaction.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Budgeting.Models.Accounts
@@ -9,6 +10,8 @@ namespace Budgeting.Models.Accounts
         public string Name { get; set; } = null!;
         public string? Description { get; set; }
         public decimal Amount { get; set; }
+        [Comment("If the transaction affected the holding price/shares.")]
+        public bool ModifiedHolding { get; set; } = true;
 
         public long ToHoldingTransactionId { get; set; }
         public long FromHoldingTransactionId { get; set; }
