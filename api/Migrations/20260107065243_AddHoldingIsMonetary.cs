4ab2003f09b874bdd650a91a14b491c80118b75c
Andy Situ <and.situ@gmail.com>
2026-01-06 22:53:18 -0800

    DB: add IsMonetary to Holding

---
+++ api/Migrations/20260107065243_AddHoldingIsMonetary.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHoldingIsMonetary : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsMonterary",
+                table: "Holdings",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsMonterary",
+                table: "Holdings");
+        }
+    }
+}
