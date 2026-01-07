c0e643aaa67e5dd1467d3feb209a0c27aaeed731
Andy Situ <and.situ@gmail.com>
2026-01-06 23:27:07 -0800

    DB: fix typo in IsMonetary column

---
+++ api/Migrations/20260107072648_FixTypoForColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixTypoForColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsMonterary",
+                table: "Holdings",
+                newName: "IsMonetary");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsMonetary",
+                table: "Holdings",
+                newName: "IsMonterary");
+        }
+    }
+}
--- api/models/accounts/Holding.cs
+++ api/models/accounts/Holding.cs
@@ -9,7 +9,7 @@ public class Holding : BaseEntity
     public required string Name { get; set; }
     public decimal Shares { get; set; } = 0;
     public decimal Price { get; set; } = 1;
-    public bool IsMonterary { get; set; } = false;
+    public bool IsMonetary { get; set; } = false;
     public required long AccountId { get; set; }
     [ForeignKey(nameof(AccountId))]
     public virtual Account? Account { get; set; }
