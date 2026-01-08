68131215098052d8512a28ad8c5170cc82d6a74e
Andy Situ <and.situ@gmail.com>
2026-01-07 23:50:53 -0800

    Create CheckboxListItem.tsx

---
+++ ui/app/components/inputs/CheckboxListItem.tsx
@@ -0,0 +1,49 @@
+import { isEmptyObject } from "@/lib/common/util";
+import styles from "./textlistitem.module.css";
+import ListItem from "./ListItem";
+import { forwardRef } from "react";
+
+interface CheckboxListItemProps
+  extends Omit<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    "onChange" | "value"
+  > {
+  label: string;
+  value: boolean;
+  onChange: (value: boolean) => void;
+  containerStyle?: Record<string, any>;
+  // For rest of props going to input
+  [key: string]: any;
+}
+
+const CheckboxListItem = forwardRef<HTMLInputElement, CheckboxListItemProps>(
+  function CheckboxListItemWithRef(
+    { label, value, onChange, containerStyle, ...rest },
+    ref
+  ) {
+    const divStyle: React.CSSProperties = {
+      display: "grid",
+      gridTemplateColumns: "1fr 1fr",
+    };
+
+    if (!isEmptyObject(containerStyle)) {
+      Object.assign(divStyle, containerStyle);
+    }
+
+    return (
+      <ListItem label={label} containerStyle={divStyle}>
+        <input
+          checked={value}
+          onChange={(e) => {
+            onChange(e.target.checked);
+          }}
+          type={"checkbox"}
+          ref={ref}
+          {...rest}
+        />
+      </ListItem>
+    );
+  }
+);
+
+export default CheckboxListItem;
