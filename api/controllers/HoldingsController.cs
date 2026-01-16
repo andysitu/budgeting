0d76e3c6d92dd1776c73e4035d6f5fbc56efe555
Andy Situ <and.situ@gmail.com>
2026-01-15 22:20:56 -0800

    API: fetch holding transactions

--- api/controllers/HoldingsController.cs
+++ api/controllers/HoldingsController.cs
@@ -13,6 +13,24 @@ public class HoldingTransferDto
     public long to_shares { get; set; }
 }
 
+public class TransactionDto
+{
+    public long id { get; set; }
+    public string name { get; set; }
+    public string description { get; set; }
+    public decimal amount { get; set; }
+    public bool modified_holding { get; set; }
+}
+
+public class HoldingTransactionDto
+{
+    public long id { get; set; }
+    public decimal shares { get; set; }
+    public decimal price { get; set; }
+    public TransactionDto? source_transaction { get; set; }
+    public TransactionDto? destination_transaction { get; set; }
+}
+
 [Authorize]
 [ApiController]
 [Route("holdings")]
@@ -99,5 +117,63 @@ public class HoldingsController : Controller
         }
 
         return Ok();
+    }
+
+    private List<HoldingTransactionDto> MapHoldingTransactionDto(List<HoldingTransaction> holdingTransactions)
+    {
+        List<HoldingTransactionDto> dtos = new();
+
+        foreach (HoldingTransaction holdingTransaction in holdingTransactions)
+        {
+            HoldingTransactionDto holdingTransactionDto = new()
+            {
+                id = holdingTransaction.Id,
+                shares = holdingTransaction.Shares,
+                price = holdingTransaction.Price,
+            };
+
+            if (holdingTransaction.SourceTransaction != null)
+            {
+                Transaction t = holdingTransaction.SourceTransaction;
+                TransactionDto sourceTransaction = new()
+                {
+                    id = t.Id,
+                    name = t.Name,
+                    description = t.Description,
+                    amount = t.Amount,
+                    modified_holding = t.ModifiedHolding,
+                };
+                holdingTransactionDto.source_transaction = sourceTransaction;
+            }
+
+            if (holdingTransaction.DestinationTransaction != null)
+            {
+                Transaction t = holdingTransaction.DestinationTransaction;
+                TransactionDto destTransaction = new()
+                {
+                    id = t.Id,
+                    name = t.Name,
+                    description = t.Description,
+                    amount = t.Amount,
+                    modified_holding = t.ModifiedHolding,
+                };
+                holdingTransactionDto.destination_transaction = destTransaction;
+            }
+            dtos.Add(holdingTransactionDto);
+        }
+
+        return dtos;
+    }
+
+    [Authorize]
+    [HttpGet("{holdingId}/transactions")]
+    public async Task<List<HoldingTransactionDto>> GetHoldingTransactions(int holdingId)
+    {
+        var holdingTransactions = await _context.HoldingTransactions
+            .Include(ht => ht.SourceTransaction)
+            .Include(ht => ht.DestinationTransaction)
+            .Where(ht => ht.HoldingId == holdingId)
+            .ToListAsync();
+        return MapHoldingTransactionDto(holdingTransactions);
     }
 }
