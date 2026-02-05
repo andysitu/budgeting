9cb58e26e9ea615d0ee68a71be163b265f8d7670
Andy Situ <and.situ@gmail.com>
2026-02-04 23:04:20 -0800

    UI: fix getting today in DateTimeListItem

--- ui/app/components/inputs/DateTimeListItem.tsx
+++ ui/app/components/inputs/DateTimeListItem.tsx
@@ -23,9 +23,16 @@ function DateTimeListItem({
   onChangeTime,
 }: DateTimeListItemProps) {
   const setToToday = () => {
-    const today = new Date().toISOString();
-    onChangeDate(today.split("T")[0]);
-    onChangeTime(today.split("T")[1].split(".")[0]);
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, "0");
+    const day = String(now.getDate()).padStart(2, "0");
+
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const seconds = String(now.getSeconds()).padStart(2, "0");
+    onChangeDate(`${year}-${month}-${day}`);
+    onChangeTime(`${hours}:${minutes}:${seconds}`);
   };
   return (
     <ListItem
