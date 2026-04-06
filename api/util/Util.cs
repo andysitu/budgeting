f2ac91d12452245348d23f62d495cd4c4437a656
Andy Situ <and.situ@gmail.com>
2026-04-05 22:49:55 -0700

    Move out of namespace for Util

--- api/util/Util.cs
+++ api/util/Util.cs
@@ -1,14 +1,13 @@
 using System.Security.Claims;
 using Microsoft.AspNetCore.Identity;
 
-namespace Budget.Util
+namespace Budget.Util;
+
+public static class Util
 {
-    public static class Util
+    public static string? getCurrentUserId(HttpContext httpContext)
     {
-        public static string? getCurrentUserId(HttpContext httpContext)
-        {
-            // ClaimTypes.NameIdentifier gets the identifier (user in this case)
-            return httpContext.User?.FindFirstValue(ClaimTypes.NameIdentifier);
-        }
+        // ClaimTypes.NameIdentifier gets the identifier (user in this case)
+        return httpContext.User?.FindFirstValue(ClaimTypes.NameIdentifier);
     }
 }
